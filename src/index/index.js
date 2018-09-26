ad2b3aebd99eb4fb07cc9216366796022acc55df
newlifebegins <1345795498@qq.com>
2018-09-26 20:47:58 +0800

    init

--- src/index/index.js
+++
@@ -1,5 +0,0 @@
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'font-awesome/css/font-awesome.min.css';
-// import '../public/bootstrap/css/bootstrap.min.css'
-import 'bootstrap-loader'
-import './index.css'
