613d81b0454ef3551f503e73142a67330d4ac73c
newlifebegins <1345795498@qq.com>
2018-09-26 17:59:17 +0800

    init

--- src/index/index.js
+++ src/index/index.js
@@ -1,2 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.min.css';
+// import '../public/bootstrap/css/bootstrap.min.css'
+import 'bootstrap-loader'
+import './index.css'
